1734d6c7dc4765b859aa22ae4107fb212903231a
demichele-c <demichele.charles@yahoo.com>
2024-10-09 00:42:27 -0400

    backend initial file structure

---
+++ server/schemas/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  message: "Schemas loaded successfully!"
+};
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const { gql } = require('apollo-server-express');
+
+// Define your GraphQL schema using gql
+const typeDefs = gql`
+  # Define the User type
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  # Define the Itinerary type
+  type Itinerary {
+    id: ID!
+    description: String!
+    date: String!
+    location: String!
+  }
+
+  # Define the Query type
+  type Query {
+    users: [User]
+    user(id: ID!): User
+    itineraries: [Itinerary]
+    itinerary(id: ID!): Itinerary
+  }
+
+  # Define the Mutation type
+  type Mutation {
+    addUser(name: String!, email: String!): User
+    addItinerary(description: String!, date: String!, location: String!): Itinerary
+  }
+`;
+
+module.exports = typeDefs;
