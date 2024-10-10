54650efcdff5a2d16a5a187e05aba512b8ea88c9
David Villarreal <djmarch92014@gmail.com>
2024-10-09 19:43:20 -0500

    Created the function to create an encrypted password and a function to check if a password was entered correctly.

--- server/models/User.js
+++ server/models/User.js
@@ -23,6 +23,26 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+// set up pre-save middleware to create password
+// We want to run the function before the document is saved
+userSchema.pre('save', async function (next){
+  // if this is a new user or the password has been changed
+  // we want to create an encrypted password
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  // This releases the hook because nothing happens until this call
+  next();
+});
+
+// This function will get the current password that the user typed in and the eccrypted password in the database
+userSchema.methods.isCorrectPassword = async function (password) {
+  // .compare will return true or false
+  // Create a custom method to compare the passed in password with the hashed password 
+  return bcrypt.compare(password, this.password);
+};
+
 // Default empty array for itineraries
 userSchema.pre('save', function (next) {
   if (!this.itineraries) {
