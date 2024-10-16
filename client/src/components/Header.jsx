b9975379ba471f44eb37a7303fccc6284799033c
Cassidy Pagels <rozeneclipse@gmail.com>
2024-10-15 19:35:38 -0500

    menu dropdown functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,64 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import * as React from 'react';
+import { AppBar, Toolbar, Typography, Button, Menu, MenuItem} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   return (
     <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        <Typography align="left" variant="h6" color="inherit" component={Link} to="/" sx={{ flexGrow: 1 }}>
           ItinerAi
         </Typography>
-        <Box>
-          <Button color="inherit" component={Link} to="/" sx={{ mx: 1 }}>
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/itineraries" sx={{ mx: 1 }}>
-            Itineraries
-          </Button>
-          <Button color="inherit" component={Link} to="/profile" sx={{ mx: 1 }}>
-            Profile
-          </Button>
-          <Button color="inherit" component={Link} to="/login" sx={{ mx: 1 }}>
-            Login
-          </Button>
-        </Box>
+        <MenuIcon 
+          onClick={handleMenu}
+        />
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onclose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <Button component={Link} to="/itineraries" sx={{ mx: 1 }}>
+              Itineraries
+            </Button>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Button component={Link} to="/profile" sx={{ mx: 1 }}>
+              Profile
+            </Button>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Button component={Link} to="/login" sx={{ mx: 1 }}>
+              Login
+            </Button>
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
 };
+
 
 export default Header;
