2b0957e6a1494c050683e76a78a35fe5297ee024
Rich <richardstrain@gmail.com>
2024-10-24 14:57:43 -0400

    Added Second Food Preference

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -68,64 +68,16 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     createCheckoutSession(userId: ID!): CheckoutSession
     confirmUpgrade(sessionId: String!): Auth
-    aiResponse(itLocation: String, itDate: String, itCelebration: String, itInterests: String, itFoodPreferences: String, itTimeRange: String): AIResponse
+    aiResponse(
+      itLocation: String
+      itDate: String
+      itCelebration: String
+      itInterests: String
+      itFoodPreferences: String
+      itSecondFoodPreference: String
+      itTimeRange: String
+    ): AIResponse
     deleteItinerary(id: ID!): User
   }
 `;
 module.exports = typeDefs;
-// // module.exports = typeDefs;
-// const { gql } = require('apollo-server-express');
-
-// // Define your GraphQL schema using gql
-// const typeDefs = gql`
-//   # Define the User type
-//   type User {
-//     username: String
-//     password: String
-//     id: ID
-//     name: String
-//     email: String
-//     isUpgraded: Boolean
-//   }
-
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
-
-//   # Define the Itinerary type
-//   type Itinerary {
-//     id: ID!
-//     description: String!
-//     date: String!
-//     location: String!
-//   }
-
-//   type CheckoutSession {
-//     id: String!
-//   }
-
-//   type aiResponse {
-//     content: String
-//   }
-
-//   # Define the Query type
-//   type Query {
-//     users: [User]
-//     user(id: ID!): User
-//     itineraries: [Itinerary]
-//     itinerary(id: ID!): Itinerary
-//   }
-
-//   # Define the Mutation type
-//   type Mutation {
-//     addItinerary(description: String!, date: String!, location: String!): Itinerary
-//     addProfile(username: String!, email: String!, password: String!, name: String!): Auth
-//     upgradeUser(userId: ID!, isUpgraded: Boolean): User
-//     login(email: String!, password: String!): Auth
-//     createCheckoutSession(userId: ID!): CheckoutSession
-//     aiResponse(itLocation: String, itDate: String, itCelebration: String, itInterests: String, itFoodPreferences: String, itTimeRange: String): aiResponse
-//   }
-// `;
-
-// module.exports = typeDefs;
